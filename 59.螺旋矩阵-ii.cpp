e241c78f62132b2d071f1bf610837fc5fdd6f68e
非栓 铨 <a502230347@qq.com>
2019-09-20 01:18:19 +0800

    '59'

--- 59.螺旋矩阵-ii.cpp
+++ 59.螺旋矩阵-ii.cpp
@@ -1,44 +1,42 @@
 /*
- * @lc app=leetcode.cn id=54 lang=cpp
+ * @lc app=leetcode.cn id=59 lang=cpp
  *
- * [54] 螺旋矩阵
+ * [59] 螺旋矩阵 II
  */
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int>ans;
-        if(matrix.empty())
-            return ans;
-        //border side边界
+    vector<vector<int>> generateMatrix(int n) {
+        //vector<vector<int>>v(n,vector<int>(n,0));
+        vector<vector<int>>v(n, vector<int>(n, 0));
+        //border side
         int u=0;
-        int d=matrix.size()-1;
+        int d=n-1;
         int l=0;
-        int r=matrix[0].size()-1;
+        int r=n-1;
+        int elem=1;
         while(true)
         {
             //go right
             for(int i=l;i<=r;++i)
-                ans.push_back(matrix[u][i]);
+                v[u][i]=elem++;
             if(++u>d)
                 break;
             //go down
             for(int i=u;i<=d;++i)
-                ans.push_back(matrix[i][r]);
+                v[i][r]=elem++;
             if(--r<l)
                 break;
             //go left
             for(int i=r;i>=l;--i)
-                ans.push_back(matrix[d][i]);
+                v[d][i]=elem++;
             if(--d<u)
                 break;
             //go up
             for(int i=d;i>=u;--i)
-                ans.push_back(matrix[i][l]);
+                v[i][l]=elem++;
             if(++l>r)
                 break;
         }
-        return ans;
+        return v;
     }
 };
-
-
