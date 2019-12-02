109c3cf9f9e3e18a9076d3639abe8ef12b9deba5
非栓 铨 <a502230347@qq.com>
2019-12-03 00:53:43 +0800

    bit calcuation

---
+++ 231.2-的幂.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=231 lang=cpp
+ *
+ * [231] 2的幂
+ */
+
+// @lc code=start
+class Solution {
+public:
+    // bool isPowerOfTwo(int n) {
+    //     int sum=0;
+    //     //需要n>0因为如果是负数的话，右移左边添加的是1，无穷循环
+    //     while(n>0){
+    //         sum=sum+(n&1);
+    //         n=n>>1;
+    //     }
+    //     return sum==1;
+    // }
+        bool isPowerOfTwo(int n) {
+            return (n>0) && (n&(n-1))==0;
+    }
+};
+// @lc code=end
+
